10310acddec64e1161b98df8916c83e61fd9e711
LuxBrush <82244625+LuxBrush@users.noreply.github.com>
2025-08-04 13:01:57 -0700

    refactor: Update for Manifest V3 compatibility
    - Replace deprecated chrome.tabs.executeScript with chrome.scripting.executeScript
    - Update chrome.browserAction to chrome.action API
    - Add scripting permission to manifest.json
    - Remove unused popup-related code (createPage and related functions)
    - Add null checks for tab IDs
    - Modernize YouTube progress tracking implementation

--- background.js
+++ background.js
@@ -216,7 +216,13 @@ chrome.tabs.onUpdated.addListener(function (_tabId, changeInfo, updatedTab) {
 			//console.log("Replace state and stuff");
 
 			//Loads the page without the ABUid
-			chrome.tabs.executeScript(updatedTab.id, { code: "location.replace('" + newURL + "');", runAt: "document_start" });
+			if (updatedTab.id) {
+				chrome.scripting.executeScript({
+					target: { tabId: updatedTab.id },
+					func: (/** @type {string} */ url) => location.replace(url),
+					args: [newURL],
+				});
+			}
 
 			//history.replaceState({},'',newURL);
 			//location.reload();
@@ -259,27 +265,31 @@ function updateTabInfo(thisTab) {
 		//We cannot run functions, like document.getElementById("movie_player").getCurrentTime(), but we can read values. So we have to use a roundabout method to get what we want; the best seems to be getting the aria-valuenow from ytp-progress-bar
 
 		// As a video progresses, automatically adds
-		chrome.tabs.executeScript(thisTab.id, {
-			allFrames: true,
-			code: `
-			if(!ABUYT){
-				var ABUYT = setInterval(function(){
-					// console.log('RUNNING INTERVAL');
-					var progressBar = document.getElementsByClassName("ytp-progress-bar");
-					
-					if(!progressBar.length) return;
-					
-					// If a miniplayer is opened, we need to make sure we get the last element- that will be the main player.
-					var newURL = window.location.href.replace(/&t=[^&]+|$/,"&t="+progressBar[progressBar.length-1].getAttribute("aria-valuenow"));
-					
-					// Don't update the history if it's the same- this wastes resources
-					if(newURL === window.location.href) return;
-					
-					history.replaceState(null,'',newURL);
-				},1000);
-			}
-		`,
-		});
+		if (thisTab.id) {
+			chrome.scripting.executeScript({
+				target: { tabId: thisTab.id, allFrames: true },
+				func: () => {
+					// @ts-ignore
+					if (!window.ABUYT) {
+						// @ts-ignore
+						window.ABUYT = setInterval(function () {
+							// console.log('RUNNING INTERVAL');
+							const progressBar = document.getElementsByClassName("ytp-progress-bar");
+
+							if (!progressBar.length) return;
+
+							// If a miniplayer is opened, we need to make sure we get the last element- that will be the main player.
+							const newURL = window.location.href.replace(/&t=[^&]+|$/, "&t=" + progressBar[progressBar.length - 1].getAttribute("aria-valuenow"));
+
+							// Don't update the history if it's the same- this wastes resources
+							if (newURL === window.location.href) return;
+
+							history.replaceState(null, "", newURL);
+						}, 1000);
+					}
+				},
+			});
+		}
 	}
 
 	chrome.storage.sync.get(function (/** @type {StorageObject} */ storage) {
@@ -333,340 +343,6 @@ function updateTabInfo(thisTab) {
 			}
 		}
 	});
-}
-
-function createPage() {
-	chrome.storage.sync.get(function (/** @type {StorageObject} */ storage) {
-		//ABUVersion info
-		if (!storage.ABUVersion || storage.ABUVersion < ABUVersion) {
-			document.getElementsByTagName("BODY")[0].insertAdjacentHTML("afterbegin", "<p id='update'>ABU 1.4 adds support for mangahub.io. Always feel free to let me know if ABU doesn't work on any website!</p>");
-			chrome.storage.sync.set({ "ABUVersion": ABUVersion });
-		}
-
-		const mainButton = Get.elementByID("current-page");
-		mainButton.dataset.multiple = "0";
-		//console.log(domain,domain.substr(0,domain.length-2).indexOf("/")==-1);
-
-		console.log("url is", url);
-
-		//If we're on the homepage, warn the user that subpages are better
-		if (
-			// If all of these are true, the user's on a homepage
-			(domain.indexOf("/") !== -1 && domain.substring(0, domain.length - 2).indexOf("/") == -1 && !/(page|p|date)=/i.test(url) && !/tapas.io\/(series|episode)\//.test(url)) ||
-			// If any of these are true, the user's on a homepage
-			/mangahub.io\/manga\//.test(url)
-		) {
-			if (warning.indexOf("a subpage if") === -1)
-				warning += "ABUkmark a subpage if possible so visiting about, archives, links, etc doesn't update bookmarks. Just click on an article, a back button, or a button to start reading and it should be perfect!<br>";
-		}
-
-		let anywhereButtonsString = "";
-
-		domain = resolveUrlPath(storage, domain);
-
-		//Setup buttons
-		if (!storage[domain]) {
-			//If we don't have an ABUkmark for this site
-			chrome.bookmarks.search(domain, function (thisBookmark1) {
-				if (thisBookmark1[0]) {
-					const firstBookmark = thisBookmark1[0];
-					if (!firstBookmark.url) {
-						throw new Error("Bookmark URL is undefined or null");
-					}
-					//If the bookmark exists
-					mainButton.innerHTML = "Convert to ABUkmark";
-					mainButton.style.backgroundColor = "#9ccc5e";
-
-					overwriteWarning(firstBookmark);
-
-					setNotification(warning + "Will convert <em title='" + firstBookmark.url + "'>" + firstBookmark.title + "</em>. <span id='onlyNewABU'>Or, make a new ABUkmark.</span>", ABU, domain);
-					if (thisBookmark1.length > 1) {
-						let bookmarksChoose = "";
-
-						warningClass = "";
-
-						//Create a dropdown so you can choose which to change
-						for (let i = 0; i < thisBookmark1.length; i++) {
-							const currentBookmark = thisBookmark1[i];
-							if (!currentBookmark.url) {
-								throw new Error("Current Bookmark URL is undefined or null");
-							}
-							warningClass = "";
-
-							overwriteWarning(thisBookmark1[i]);
-
-							const dropdownDomain = resolveUrlPath(storage, normalizeContentUrl(currentBookmark.url, currentBookmark.title, storage)); //checkLevels(thisBookmark1[i].url);
-
-							//console.log(dropdownDomain);
-
-							//Add a dropdown with the bookmarks info
-							if (currentBookmark.title.indexOf(" (ABU)") === -1) {
-								//If the bookmark is untitled, let the user know
-								let thisBookmarkTitle = currentBookmark.title;
-								if (thisBookmarkTitle == "") {
-									thisBookmarkTitle = "(Untitled)";
-								}
-
-								bookmarksChoose += "<option class='" + warningClass + "' title='" + currentBookmark.url + "' data-domain='" + dropdownDomain + "' value='" + currentBookmark.id + "'>" + thisBookmarkTitle + "</option>";
-							} else {
-								bookmarksChoose += "<option class='overwrite' title='" + currentBookmark.url + "' data-domain='" + dropdownDomain + "' value='" + currentBookmark.id + "'>" + currentBookmark.title + "</option>";
-							}
-						}
-						setNotification(warning + thisBookmark1.length + " bookmarks spotted. Will convert <select>" + bookmarksChoose + "</select>. <span id='onlyNewABU'>Or, make a new ABUkmark.</span>", ABU, domain);
-						mainButton.dataset.multiple = "1";
-					}
-				} else {
-					//If it doesn't
-					//Change the button's text and color
-					mainButton.innerHTML = "Create ABUkmark";
-					mainButton.style.backgroundColor = "#619919";
-					//Set the notification if there's a warning
-					if (warning !== "") {
-						setNotification(warning, ABU, domain);
-					}
-				}
-				mainButton.onclick = function () {
-					ABU(domain, false);
-				};
-			});
-		} else {
-			const domainData = storage[domain];
-			if (!domainData) {
-				throw new Error("No ABUkmark found for this domain in storage");
-			}
-			//If we have an ABUkmark for this, according to our data
-			chrome.bookmarks.search("ABUid=" + domainData.ABUid, function (thisBookmark2) {
-				if (!thisBookmark2) {
-					chrome.storage.sync.remove(domain);
-				} else {
-					let check = false;
-					for (const bookmarkResult of thisBookmark2) {
-						if (!bookmarkResult.url) {
-							console.error("Bookmark result URL is undefined or null");
-							continue;
-						}
-						if (bookmarkResult && domain === resolveUrlPath(storage, normalizeContentUrl(bookmarkResult.url, bookmarkResult.title, storage))) {
-							check = true;
-						}
-					}
-
-					//GO THROUGH THE FOR LOOP (otherwise won't work with multiple pages and if in a higher-level domain; need to check for that)
-
-					if (thisBookmark2[0] && check) {
-						//If we've found out the bookmark claimed to exist does, set the button so that:
-						mainButton.innerHTML = "Revert to normal bookmark";
-						mainButton.style.backgroundColor = "#f00";
-						mainButton.onclick = function () {
-							const domainData = storage[domain];
-							if (!domainData) {
-								throw new Error("Domain data not found in storage for unABU operation");
-							}
-							unABU(domain, domainData.ABUid);
-							chrome.browserAction.setIcon({ path: inactiveIcons });
-						};
-					} else {
-						chrome.storage.sync.remove(domain);
-						mainButton.innerHTML = "Create ABUkmark";
-						mainButton.style.backgroundColor = "#619919";
-						mainButton.onclick = function () {
-							ABU(domain, false);
-						};
-					}
-				}
-			});
-		}
-
-		let storageKeys = Object.keys(storage);
-
-		// Create buttons for removing ABUkmarks
-		for (const key of storageKeys) {
-			// Don't create a button for the webpage domain we're on
-			// Don't create a button for the ABUVersion object, which checks the current version in use.
-			if (key === domain || key === "ABUVersion") {
-				continue;
-			}
-			const abuBookmark = storage[key];
-			if (!abuBookmark) {
-				throw new Error(`Storage data not found for key: ${key}`);
-			}
-
-			// Look for the bookmarks as we go through the list, to make sure they still exist.
-			chrome.bookmarks.search(`ABUid=${abuBookmark.ABUid}`, (bookmarks) => {
-				// If a bookmark in the list doesn't exist
-				if (bookmarks.length === 0) {
-					const ABUid = abuBookmark.ABUid;
-
-					// Remove the info
-					chrome.storage.sync.remove(key);
-
-					// Remove the element, if it exists
-					const abuButton = document.querySelector(`button[data-id="${ABUid}"]`);
-					if (abuButton) abuButton.remove();
-				}
-			});
-
-			// Add the button if it exists
-			anywhereButtonsString += `<button data-id="${abuBookmark.ABUid}" data-domain="${key}">&times; <img src="${abuBookmark.favIconUrl}"> ${key}</button>`;
-		}
-
-		//If the user doesn't have any ABUkmarks, don't show the horizontal rule
-		if (anywhereButtonsString == "") {
-			document.getElementsByTagName("hr")[0].style.display = "none";
-		}
-
-		console.log("Any favicons not found will produce errors below (it's not really worth worrying about)");
-
-		const anywhereDiv = document.getElementById("abu-anywhere");
-		if (!anywhereDiv) {
-			throw new Error("Element with ID 'abu-anywhere' not found");
-		}
-
-		anywhereDiv.innerHTML = anywhereButtonsString;
-
-		let anywhereButtons = /** @type {HTMLCollectionOf<HTMLButtonElement>} */ (anywhereDiv.children);
-		let images = document.getElementsByTagName("img");
-
-		// Add functions for each button
-		for (let ii = 0; ii < anywhereButtons.length; ii++) {
-			const anywhereButton = anywhereButtons[ii];
-			const domainPath = anywhereButton.dataset.domain;
-			const abuId = anywhereButton.dataset.id;
-			if (!domainPath || !abuId) {
-				console.error("Domain or ID missing from button data attributes");
-				continue;
-			}
-			anywhereButton.onclick = () => {
-				unABU(domainPath, Number(abuId));
-			};
-
-			//Hide any images that fail to load properly
-			const image = images[ii];
-			image.onerror = () => {
-				image.style = "display:none;";
-			};
-		}
-	});
-}
-
-/**
- * Checks if a bookmark is an ABUkmark and adds a warning message if it is.
- *
- * This function is called when displaying bookmarks in the popup interface.
- * It detects if a bookmark is already an ABUkmark (has "(ABU)" in the title)
- * and adds a warning message to prevent users from accidentally overwriting
- * existing ABUkmarks. It also sets the warningClass to "overwrite" which
- * applies special styling to highlight the warning.
- *
- * @param {chrome.bookmarks.BookmarkTreeNode} bookmark - The bookmark object to check
- */
-function overwriteWarning(bookmark) {
-	if (bookmark.title.indexOf(" (ABU)") !== -1 && warning.indexOf("overwrite") == -1) {
-		warningClass = "overwrite";
-		warning += "<strong>Don't accidentally overwrite ABUkmarks deeper in the website!</strong> If you do it, do it on purpose. Any bookmarks ending in (ABU) are ABUkmarks.<br>";
-	}
-}
-
-/**
- * Creates or updates an ABUkmark for the specified domain.
- * Converts existing bookmarks to ABUkmarks or creates new ones.
- *
- * @param {string} domainPath - The processed domain path (e.g., "example.com/blog/")
- * @param {boolean} forceCreateNew - If true, creates new ABUkmark even if bookmark exists
- * @returns {void}
- *
- * @example
- * ABU("example.com/blog/", false); // Convert existing bookmark
- * ABU("example.com/blog/", true);  // Force create new ABUkmark
- */
-function ABU(domainPath, forceCreateNew) {
-	const mainButton = Get.elementByID("current-page");
-	let inArray = 0;
-	let inArrayDomain = "";
-
-	if (mainButton.dataset.multiple === "1") {
-		const selectElement = /** @type {HTMLSelectElement} */ (document.getElementsByTagName("SELECT")[0]);
-		inArray = selectElement.selectedIndex;
-
-		//Get the bookmark to change with this:
-		if (selectElement.options[selectElement.selectedIndex].className !== "") {
-			const domainString = selectElement.options[selectElement.selectedIndex].dataset.domain;
-			if (!domainString) {
-				console.error("Domain string is undefined or null for selected bookmark");
-				return;
-			}
-			inArrayDomain = domainString;
-		}
-	}
-
-	chrome.bookmarks.search(domainPath, function (thisBookmark) {
-		if (!thisBookmark[inArray] || forceCreateNew) {
-			//If the bookmark doesn't exist
-
-			//Check for ABUkmarks folder, add if doesn't exist
-			chrome.bookmarks.search("ABUkmarks", function (thisFolder) {
-				if (!thisFolder[0]) {
-					//If folder ABUkmarks doesn't exist
-					chrome.bookmarks.create({ title: "ABUkmarks" }, function (newFolder) {
-						createABUkmark(domainPath, newFolder.id);
-					});
-				} else {
-					//If the folder exists
-					createABUkmark(domainPath, thisFolder[0].id);
-				}
-				//Not always located there; get exact location and state it
-				setNotification("New ABUkmark located in <em>Other bookmarks &#8594; ABUkmarks</em>", ABU, domain);
-			});
-		} else {
-			//If the bookmark exists
-
-			if (inArrayDomain !== "") {
-				chrome.storage.sync.remove(inArrayDomain);
-			}
-
-			let ABUid = Date.now();
-
-			storeABUkmark(domainPath, ABUid);
-
-			chrome.bookmarks.update(thisBookmark[inArray].id, { title: title + " (ABU)", url: createABURL(url, ABUid) });
-
-			setNotification("", ABU, domain);
-		}
-	});
-
-	chrome.browserAction.setIcon({ path: activeIcons });
-}
-
-/**
- * Creates a new ABUkmark (Automatic Bookmark Updater bookmark) in the browser's bookmarks.
- * The bookmark will have "(ABU)" appended to its title and a unique ID added to the URL.
- *
- * @param {string} urlIdentifier - The URL or identifier to be used for creating the ABUkmark.
- * @param {string} parentId - The ID of the parent folder where the new bookmark will be created.
- * @returns {void}
- */
-function createABUkmark(urlIdentifier, parentId) {
-	const ABUid = Date.now();
-	chrome.bookmarks.create({ parentId, title: `${title} (ABU)`, url: createABURL(url, ABUid) }, function () {
-		storeABUkmark(urlIdentifier, ABUid);
-	});
-}
-
-/**
- * Stores ABUkmark data in Chrome's synced storage.
- *
- * @param {string} domainPath - The domain path key for the ABUkmark (e.g., "example.com/blog/")
- * @param {number} abuId - The unique ABU identifier timestamp
- * @returns {void}
- *
- * @example
- * storeABUkmark("example.com/blog/", 1672531200000);
- */
-function storeABUkmark(domainPath, abuId) {
-	/** @type {StorageObject} */
-	const storageObj = {};
-	storageObj[domainPath] = { "ABUid": abuId, "favIconUrl": favIconUrl };
-	chrome.storage.sync.set(storageObj);
-	createPage();
 }
 
 function Check() {
--- script.js
+++ script.js
@@ -320,7 +320,7 @@ function updateTabInfo(thisTab) {
 
 						//If this new tab is the active one, update the icon:
 						if (thisTab.active) {
-							chrome.browserAction.setIcon({ path: activeIcons });
+							chrome.action.setIcon({ path: activeIcons });
 						}
 					}
 				}
@@ -329,7 +329,7 @@ function updateTabInfo(thisTab) {
 			//If this webpage doesn't have an associated ABUkmark
 			//If this new tab is the current one, update the icon:
 			if (thisTab.active) {
-				chrome.browserAction.setIcon({ path: inactiveIcons });
+				chrome.action.setIcon({ path: inactiveIcons });
 			}
 		}
 	});
@@ -462,7 +462,7 @@ function createPage() {
 								throw new Error("Domain data not found in storage for unABU operation");
 							}
 							unABU(domain, domainData.ABUid);
-							chrome.browserAction.setIcon({ path: inactiveIcons });
+							chrome.action.setIcon({ path: inactiveIcons });
 						};
 					} else {
 						chrome.storage.sync.remove(domain);
@@ -633,7 +633,7 @@ function ABU(domainPath, forceCreateNew) {
 		}
 	});
 
-	chrome.browserAction.setIcon({ path: activeIcons });
+	chrome.action.setIcon({ path: activeIcons });
 }
 
 /**
